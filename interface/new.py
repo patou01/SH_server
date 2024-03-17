e3f8953017a7a05aa4e18c947f7aec85212b66c9
Patoux01 <not@needed.com>
2024-03-17 18:32:41 +0100

    make it with 2 plots and qcombos with callbacks. Code is ugly af. Need to make 'add plot' button

--- interface/new.py
+++ interface/new.py
@@ -13,9 +13,17 @@ from interface.data_fetcher import CsvFetcher
 logging.basicConfig(level=logging.INFO)
 
 
+
+
+
+
 class MainWindow(QMainWindow):
-    def __init__(self, timestamps, data):
+    def __init__(self):
         super(MainWindow, self).__init__()
+        self.fetcher = CsvFetcher(Path("../server/data"))
+
+        data1 = self.fetcher.fetch("bedroom", "co2")
+        data2 = self.fetcher.fetch("bedroom", "temperature")
 
         axis = DateAxisItem()
         layout = QVBoxLayout()
@@ -23,41 +31,58 @@ class MainWindow(QMainWindow):
         horiz1 = QHBoxLayout()
 
         self.graphWidget = pg.PlotWidget(axisItems={'bottom': axis})
-        self.graphWidget.plot(timestamps[0], data[0])
+        self.graphWidget.plot(data1[0], data1[1])
 
         axis2 = DateAxisItem()
         self.graphWidget2 = pg.PlotWidget(axisItems={'bottom': axis2})
-        self.graphWidget2.plot(timestamps[1], data[1])
+        self.graphWidget2.plot(data2[0], data2[1])
 
-        types = fetcher.get_available_data_types()
-
+        types = self.fetcher.get_available_data_types()
 
         widget = QWidget()
         widget.setLayout(layout)
         box1 = QComboBox()
         box1.addItems(types)
+        box1.currentTextChanged.connect(self.combo1_callback)
+
         horiz1.addWidget(box1)
         horiz1.addWidget(self.graphWidget)
 
         horiz2 = QHBoxLayout()
         box2 = QComboBox()
         box2.addItems(types)
+        box2.currentTextChanged.connect(self.combo2_callback)
         horiz2.addWidget(box2)
         horiz2.addWidget(self.graphWidget2)
-
 
         layout.addLayout(horiz1)
         layout.addLayout(horiz2)
 
         self.setCentralWidget(widget)
 
+    def combo1_callback(self, text: str):
+        self.plot(text, 0)
 
-fetcher = CsvFetcher(Path("../server/data"))
+    def combo2_callback(self, text: str):
+        self.plot(text, 1)
 
-data1 = fetcher.fetch("bedroom", "co2")
-data2 = fetcher.fetch("bedroom", "temperature")
+
+    def plot(self, item: str, index: int):
+        """
+        Fetches the data and plots.
+        """
+        timestamps, data = self.fetcher.fetch("bedroom", item)
+
+        if index == 0:
+            target = self.graphWidget
+        else:
+            target = self.graphWidget2
+
+        target.plotItem.clear()
+        target.plot(timestamps, data)
+
 
 app = QApplication(sys.argv)
-w = MainWindow([data1[0], data2[0]], [data1[1], data2[1]])
+w = MainWindow()
 w.show()
 app.exec()
