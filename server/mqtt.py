e6a16066e88bfec1cba4df330e7d81dabe1f9e54
Patoux01 <not@needed.com>
2024-03-17 08:07:35 +0100

    slightly rewrite the server part, let's see how it goes... Need to make a GUI in a day :(

--- server/mqtt.py
+++ server/mqtt.py
@@ -1,43 +1,56 @@
-import paho.mqtt.client as mqtt #import the client
+"""
+Basically listens to all subtopics from topic which is passed as script parameter. (ie topic/#)
+
+Topics are expected something like
+bedroom/temperature
+bedroom/humidity
+bedroom/co2
+
+The data is then stored in a folder called data. Inside this folder we create a "bedroom" folder under which
+each set of data is stored as .csv.
+
+In the future, this will be improved to use something like a database.
+
+"""
+import os
+from pathlib import Path
+
+import paho.mqtt.client as mqtt
 import time
 import csv
-import calendar
-import re
 import random
+import logging
+import argparse
 
 
+logging.basicConfig(level=logging.INFO)
+counter = 0
+DATA_FOLDER = Path("./data")
 
-	
+
 def temp_callback(client, userdata, message):
-        msg = str(message.payload.decode("utf-8"))
-        print("Received a msg at " + time.asctime( time.localtime(time.time()) ) )
-        print("message received " , msg)
-        print(msg.split(' , '))
-        parsed = re.findall(r"[-+]?\d*\.\d+|\d+", msg)
-        split_msg = msg.split(' , ')
+    global counter
+    counter = counter + 1
+    msg = str(message.payload.decode("utf-8"))
+    time_received = time.time()
+    time_str = time.asctime(time.localtime(time_received))
+    split_topic = message.topic.split("/")
 
-        print(split_msg)
-        print(len(split_msg))
-        print("message topic=",message.topic)
-        print("message qos=",message.qos)
-        print("message retain flag=",message.retain)
+    logging.debug(f"message info: topic {message.topic}, qos: {message.qos}, retain: {message.retain}")
+    logging.info(f" {counter}, Received message '{msg}' at {time_str} for {split_topic[-1]}")
 
+    room_path = DATA_FOLDER / split_topic[0]
+    if not room_path.exists():
+        os.makedirs(room_path)
 
-        filename = "./" + message.topic + ".csv"
+    filename = room_path / f"{split_topic[-1]}.csv"
+    mode = 'a'
+    if not os.path.exists(filename):
+        mode = 'w'
 
-        print("file is " + filename)
-        row = []
-        row.append(time.time())
-        n = len(split_msg)
-        for i in range(0, n):
-                row.append(float(split_msg[i]))
-                
-
-        f = open(filename, 'a')
+    with open(filename, mode) as f:
         writer = csv.writer(f)
-        writer.writerow(row)
-        print("wrote row " + row + " at " + time.asctime( time.localtime(time.time())) + " to file " + filename)
-        f.close()
+        writer.writerow([time_received, float(msg)])
 
 
 def on_connect(client, userdata, flags, rc):
@@ -47,28 +60,30 @@ def on_connect(client, userdata, flags, rc):
         print("Bad connection Returned code=",rc)
 
 
-random.seed()
-client_name = "poney"
-name_int = random.getrandbits(16)
-name_int_str = str(name_int)
-client_name += name_int_str
-broker_address = "192.168.0.112"
-client = mqtt.Client(client_name) #create new instance
+def main(topic):
+    random.seed()
+    client_name = "poney"
+    name_int = random.getrandbits(16)
+    name_int_str = str(name_int)
+    client_name += name_int_str
+    broker_address = "192.168.0.171"
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, client_name)
 
-client.on_message = temp_callback
-client.on_connect = on_connect
+    if not DATA_FOLDER.exists():
+        os.makedirs(DATA_FOLDER)
 
+    client.on_message = temp_callback
+    client.on_connect = on_connect
+    client.connect(broker_address)
 
-client.connect(broker_address) #connect to broker
+    client.subscribe(f"{topic}/#")
 
-client.subscribe("Patrick")
-
-
-
-print("broker address " + broker_address + " , name: " + client_name)
-client.loop_forever()
-
-
+    logging.info("broker address " + broker_address + " , name: " + client_name)
+    client.loop_forever()
 
 
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--topic")
+    args = parser.parse_args()
+    main(args.topic)
