d17ea0e9cc48e0886ac4ce251dcb18352ecd9537
patou01 <pbobbink@gmail.com>
2024-03-30 09:17:58 +0100

    more profiling

--- interface/data_fetcher.py
+++ interface/data_fetcher.py
@@ -5,6 +5,7 @@ import csv
 import logging
 from abc import ABC, abstractmethod
 from pathlib import Path
+from time import time_ns
 
 
 class DataFetcher(ABC):
@@ -29,15 +30,25 @@ class CsvFetcher(DataFetcher):
                 if file.name == f"{data_type}.csv":
                     logging.info(f"Found {file}")
                     with open(file, "r") as f:
+                        start = time_ns()
                         reader = csv.reader(f)
                         data = [row for row in reader if row]
+                        read = time_ns()
                     timestamps = [float(row[0]) for row in data]
                     points = [float(row[1]) for row in data]
+                    convert = time_ns()
+                    logging.info(f"time to read: {(read-start)/1e9}")
+                    logging.info(f"Time to convert: {(convert-read)/1e9}")
                     return [timestamps, points]
 
         logging.warning(f"Nothing found for {room} {data_type}")
 
     def get_available_data_types(self, room: str):
+        """
+        Return the different type of measurements that are available for room
+        :param room:
+        :return:
+        """
         ret_values = []
         for file in self.files:
             if file.parts[-2] == room:
